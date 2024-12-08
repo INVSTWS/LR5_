1c844cbe0344eaf677aa388d5aeaff15b6993289
SofiiaM <@lab4.com>
2024-12-08 19:06:20 +0200

    single block for menu option in line 76

--- Task2.cs
+++ Task2.cs
@@ -73,6 +73,7 @@ public class MainClass
         Console.WriteLine($"Обрано калькулятор: {calc.GetModel()}");
         
         // Вибір операції
+        ///Reorganize Console.WriteLine calls into a single block for menu options
         Console.WriteLine("Виберіть операцію:");
         Console.WriteLine("1. Додавання");
         Console.WriteLine("2. Віднімання");
